1a4469aff277ec694d82ee739cc9084dc7bb76e6
Ahmed Sukary <ahmedsukaryy@gmail.com>
2025-11-16 21:12:10 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,6 +32,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseCors("AllowReact");
 
 app.MapControllers();
 
